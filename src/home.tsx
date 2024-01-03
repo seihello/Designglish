f8a4769a46d288bf5f95ebf68966eb9dac02241d
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-01-02 18:52:47 -0800

    feat: add My page text

--- src/home.tsx
+++ src/home.tsx
@@ -5,13 +5,16 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 export default function HomePage() {
   return (
     <View className="w-full h-screen">
-      <View className="flex flex-row justify-between items-center  px-8 py-2">
+      <View className="flex flex-row justify-between items-center px-8 py-2">
         <View className="flex flex-col w-1/2 mt-24">
           <Text className="text-[32px] font-bold">Hi, Lisa!</Text>
           <Text className="text-lg">What do you want to learn today?</Text>
         </View>
-        <View className="w-16 h-16 bg-white flex justify-center items-center rounded-full">
-          <Icon name="account" color="#239CAC" size={48} />
+        <View className="flex flex-col">
+          <View className="w-14 h-14 bg-white flex justify-center items-center rounded-full">
+            <Icon name="account" color="#239CAC" size={42} />
+          </View>
+          <Text className="text-primary-main font-bold">My page</Text>
         </View>
       </View>
     </View>
