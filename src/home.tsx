408e98c933e9ceb61f3b21cf3ec6259307bab8f2
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-01-07 22:23:21 -0800

    feat: add home header

--- src/home.tsx
+++ src/home.tsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
-import { Button, View } from "react-native";
-import { supabase } from "./lib/supabase";
+import React from "react";
+import { Image, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Text from "./components/ui/text";
 
 export default function HomePage(props: any) {
-
   return (
-    <View className="h-1/2 w-full">
-      <View className="flex flex-row items-center justify-between px-8 py-2">
-        <Button title="Go to word" onPress={() => props.navigation.push("word")} />
+    <View className="flex h-screen w-full flex-col bg-primary-main">
+      <View className="flex flex-row justify-end p-4">
+        <View className="flex flex-col">
+          <View className="flex items-center justify-center rounded-full bg-white p-1">
+            <Icon name="account" color="#239CAC" size={32} />
+          </View>
+        </View>
+      </View>
+      <View className="relative px-8">
+        <Image
+          source={require("../assets/mv.png")}
+          className="absolute right-8 -bottom-4"
+        />
+        <View className="flex w-1/2 flex-col">
+          <Text className="font-dm-bold text-[32px] text-white">
+            Welcome, Lisa!
+          </Text>
+          <Text className="font-dm-bold text-lg text-white">
+            What do you want to learn today?
+          </Text>
+        </View>
       </View>
     </View>
   );
