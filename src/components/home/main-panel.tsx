ea3facf8913d98aeaed3a66e37455b1a13b2860a
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-01-07 23:45:10 -0800

    feat: create course card

--- src/components/home/main-panel.tsx
+++ src/components/home/main-panel.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { View } from "react-native";
 import Text from "../ui/text";
 import CategoryChips from "./category-chips";
+import CourseCards from "./course-cards";
 
 export default function HomeMainPanel() {
   return (
     <View className="flex flex-col items-center justify-center rounded-2xl bg-white px-4 py-6 mt-12">
       <Text className="font-dm-bold text-[28px]">Vocab being learned</Text>
       <CategoryChips />
+      <CourseCards />
     </View>
   );
 }
