b831c468c0bfe7bf6cd8d0e2517e9c3f7f3216f1
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-30 16:05:39 -0700

    feat: show categories fetched from DB

--- src/components/home/category-chips.tsx
+++ src/components/home/category-chips.tsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
+import getAllCategories from "../../lib/categories/get-all-categories";
+import Category from "../../types/category.type";
 import Text from "../ui/text";
 
-const categories = [
-  "UX / Product",
-  "UI / Graphic",
-  "3D",
-  "Game",
-  "XXXXX",
-  "YYYYY",
-];
-
 export default function CategoryChips() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const run = async () => {
+      try {
+        const categories = await getAllCategories();
+        setCategories(categories);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    run();
+  }, []);
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      <View className="flex flex-row gap-x-2 mt-2">
+      <View className="mt-2 flex flex-row gap-x-2">
         {categories.map((category, index) => (
           <View key={index} className="rounded-xl bg-primary-100 px-4 py-2">
-            <Text className="font-dm-bold text-primary-900">{category}</Text>
+            <Text className="font-dm-bold text-primary-900">
+              {category.name}
+            </Text>
           </View>
         ))}
       </View>
