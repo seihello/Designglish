188c71aeb2c11b7d6380d6a3e3878023c9af0b50
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-30 17:36:56 -0700

    feat: select a category by clicking on the category chips

--- src/components/home/category-chips.tsx
+++ src/components/home/category-chips.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, TouchableHighlight, View } from "react-native";
 import Category from "../../types/category.type";
 import Text from "../ui/text";
 
@@ -18,11 +18,17 @@ export default function CategoryChips({
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       <View className="mt-2 flex flex-row gap-x-2">
         {categories.map((category, index) => (
-          <View key={index} className="rounded-xl bg-primary-100 px-4 py-2">
-            <Text className="font-dm-bold text-primary-900">
-              {category.name}
-            </Text>
-          </View>
+          <TouchableHighlight
+            key={index}
+            onPress={() => setSelectedCategoryId(category.id)}
+            underlayColor="white"
+          >
+            <View className="rounded-xl bg-primary-100 px-4 py-2">
+              <Text className="font-dm-bold text-primary-900">
+                {category.name}
+              </Text>
+            </View>
+          </TouchableHighlight>
         ))}
       </View>
     </ScrollView>
